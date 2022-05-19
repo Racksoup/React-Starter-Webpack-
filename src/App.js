27c295cfe60559f947ac75b5fbf3456d7af1d0ca
Racksoup <rack.connor1@gmail.com>
2022-05-19 15:04:39 -0400

    React Webpack Starter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import store from './store';
+import Base from './Base.jsx';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Base />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
